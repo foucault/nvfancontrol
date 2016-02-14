26bcaa8fff8be312d8aa970f4bd9ee026abbe135
Spyros Stathopoulos <spystath@gmail.com>
2016-02-14 19:00:15 +0200

    Minimize unsafe blocks in nvctrl/lib.rs

--- src/nvctrl/lib.rs
+++ src/nvctrl/lib.rs
@@ -41,21 +41,16 @@ impl NvidiaControl {
     }
 
     fn init() -> i32 {
-        unsafe {
-            let ret = nv_init();
-            return ret as i32;
-        }
+        let ret = unsafe { nv_init() };
+        return ret as i32;
     }
 
     fn deinit() -> i32 {
-        unsafe {
-            let ret = nv_deinit();
-            return ret as i32;
-        }
+        let ret = unsafe { nv_deinit() };
+        return ret as i32;
     }
 
     pub fn get_temp(&self) -> i32 {
-
         let mut tmp = -1 as i32;
         unsafe { nv_get_temp(&mut tmp); }
         tmp
@@ -103,29 +98,25 @@ impl NvidiaControl {
     }
 
     pub fn get_version(&self) -> String {
-        unsafe {
-            let mut v: *mut c_char = mem::uninitialized();
-            nv_get_version(&mut v);
-            assert!(!v.is_null());
-            CStr::from_ptr(v as *const c_char).to_str().unwrap().to_owned()
-        }
+        let mut v: *mut c_char = unsafe { mem::uninitialized() };
+        unsafe { nv_get_version(&mut v) };
+        assert!(!v.is_null());
+        unsafe { CStr::from_ptr(v as *const c_char).to_str().unwrap().to_owned() }
     }
 
     pub fn get_utilization(&self) -> HashMap<&str, i32> {
-        unsafe {
-            let mut v: *mut c_char = mem::uninitialized();
-            nv_get_utilization(&mut v);
-            assert!(!v.is_null());
-            let res = CStr::from_ptr(v as *const c_char).to_str().unwrap();
-            let mut ret: HashMap<&str, i32> = HashMap::with_capacity(4);
-            let parts = res.split(", ");
-            for s in parts {
-                let mut split_parts = s.split("=");
-                let key = split_parts.next().unwrap();
-                let val = split_parts.next().unwrap();
-                ret.insert(key, val.parse::<i32>().unwrap());
-            }
-            ret
+        let mut v: *mut c_char = unsafe { mem::uninitialized() };
+        unsafe { nv_get_utilization(&mut v) };
+        assert!(!v.is_null());
+        let res = unsafe { CStr::from_ptr(v as *const c_char).to_str().unwrap() };
+        let mut ret: HashMap<&str, i32> = HashMap::with_capacity(4);
+        let parts = res.split(", ");
+        for s in parts {
+            let mut split_parts = s.split("=");
+            let key = split_parts.next().unwrap();
+            let val = split_parts.next().unwrap();
+            ret.insert(key, val.parse::<i32>().unwrap());
         }
+        ret
     }
 }
